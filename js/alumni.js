498c0c30f1a901b569d8ebfeb29c27080d7e60dd
ranjitha <ranjitha.ramesh10@gmail.com>
2017-02-26 01:35:06 +0530

    few other pages

---
+++ js/alumni.js
@@ -0,0 +1,27 @@
+#para
+{
+	color:#3e2723;
+}
+#sub
+{
+	font-family: 'bree serif',serif;
+	color:#222222;
+}
+i
+{
+	position:relative;
+	top:5px;
+}
+#spara
+{
+	font-size: 3vh;
+}
+@media screen and (max-width: 768px){
+  h1{
+   margin-top:75px;
+  }
+  footer{
+    display: hidden;
+}
+
+}
